5a814bf66130190b7ea0ca9134c2f8ffd30cd9f9
Mira <kruzinka@yandex.ru>
2024-06-23 23:12:25 +0300

    product page

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,11 +1,31 @@
 import NavBar from "../components/NavBar";
+import travel from "../assets/travellers.jpg";
+import styles from "./product.module.css";
 
 function Product() {
   return (
-    <div>
+    <>
       <NavBar />
-      <div>Product page</div>
-    </div>
+      <div className={styles['container']}>
+        <img src={travel} alt="can't load" className={styles['img']} />
+        <div className={styles['info-wrapper']}>
+          <h3>About Globetrotter</h3>
+          <p className={styles['text']}>
+            Globetrotter is an innovative travel app that helps you keep an
+            amazing log of your adventures around the world! With Globetrotter,
+            you can easily track all the cities and countries you&apos;ve
+            visited, creating a unique map of your travels.
+          </p>
+          <p className={styles['text']}>
+            We will help you not only preserve your brightest impressions, but
+            will also inspire new adventures, showing how much you have already
+            seen a wonderful world. With this application your the guidebook
+            will become not only a map of countries and cities, but also your
+            personal travel diary!
+          </p>
+        </div>
+      </div>
+    </>
   );
 }
 
