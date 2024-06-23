7cd0dfd00249df31c6e5d88839eb51a91e2d365b
Mira <kruzinka@yandex.ru>
2024-06-23 23:25:29 +0300

    pricing page

--- src/pages/Pricing.jsx
+++ src/pages/Pricing.jsx
@@ -1,11 +1,33 @@
-import React from "react";
 import NavBar from "../components/NavBar";
+import styles from "./product.module.css";
+import pricing from "../assets/price.jpg";
+import { useNavigate } from "react-router-dom";
 
 function Pricing() {
+  const navigate = useNavigate();
+
   return (
     <>
       <NavBar />
-      <div>Pricing</div>
+      <div className={styles["container"]}>
+        <div className={styles["info-wrapper"]}>
+          <h3>It&apos;s simple. Just $3/month</h3>
+          <p className={styles["text"]}>
+            It&apos;s a small price to pay for the opportunity to hold in your
+            hands the entire world you&apos;ve already had the chance to
+            explore. Don&apos;t miss the chance to digitize and memorize all
+            your trips, share your experiences with friends and plan new
+            adventures!
+          </p>
+          <p className={styles["text"]}>
+            It only takes a few clicks to start using Globetrotter and create
+            your own unique travel map. Join our community of travelers and give
+            new life to your adventures!
+          </p>
+          <button className={styles["app-btn"]} onClick={() => navigate('/login')}>START TRACKING NOW</button>
+        </div>
+        <img src={pricing} alt="can't load" className={styles["img"]} />
+      </div>
     </>
   );
 }
--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -6,17 +6,17 @@ function Product() {
   return (
     <>
       <NavBar />
-      <div className={styles['container']}>
-        <img src={travel} alt="can't load" className={styles['img']} />
-        <div className={styles['info-wrapper']}>
+      <div className={styles["container"]}>
+        <img src={travel} alt="can't load" className={styles["img"]} />
+        <div className={styles["info-wrapper"]}>
           <h3>About Globetrotter</h3>
-          <p className={styles['text']}>
+          <p className={styles["text"]}>
             Globetrotter is an innovative travel app that helps you keep an
             amazing log of your adventures around the world! With Globetrotter,
             you can easily track all the cities and countries you&apos;ve
             visited, creating a unique map of your travels.
           </p>
-          <p className={styles['text']}>
+          <p className={styles["text"]}>
             We will help you not only preserve your brightest impressions, but
             will also inspire new adventures, showing how much you have already
             seen a wonderful world. With this application your the guidebook
